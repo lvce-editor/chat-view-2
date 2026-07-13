4d845df678c75e5b4cc9677a8b76e69437e30a31
Le Vivilet <levivilet@pm.me>
2026-07-13 22:45:51 +0000

    fix: use mock backend in message links e2e

--- packages/e2e/src/chat2.virtual-dom-view.message-links.ts
+++ packages/e2e/src/chat2.virtual-dom-view.message-links.ts
@@ -4,6 +4,7 @@ export const name = 'chat2.virtual-dom-view.message-links'
 
 export const test: Test = async ({ Command, expect, Locator, Main }) => {
   await Main.closeAllEditors()
+  await Command.execute('Preferences.update', { 'chat2.useMockBackend': true })
   await Command.executeExtensionCommand('chat2.show')
 
   await Locator('textarea[name="composer"]').type('Inspect https://example.com')
