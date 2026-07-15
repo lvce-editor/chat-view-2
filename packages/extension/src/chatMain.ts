cc117c238da3bf3383f68954ed9205a48681deff
Le Vivilet <levivilet@pm.me>
2026-07-15 14:44:25 +0000

    fix: enable computer use in server-backed web sessions

--- packages/extension/src/chatMain.ts
+++ packages/extension/src/chatMain.ts
@@ -1,5 +1,7 @@
 /* eslint-disable unicorn/no-top-level-side-effects */
 export { activate, deactivate } from './parts/Main/Main.ts'
+import { setExtensionRuntimeFromModuleUrl } from './parts/ExtensionRuntime/ExtensionRuntime.ts'
 import { activate } from './parts/Main/Main.ts'
 
+setExtensionRuntimeFromModuleUrl(import.meta.url)
 await activate()
